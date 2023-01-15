8d98d7a302c8661eccce0da2502e6eed5001dfa6
Jfnrosimo <j.rosimo098.com>
2023-01-15 15:02:06 +0800

    Feat: price and sort state

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,46 +1,69 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 //Import components
 import List from "../components/List";
 
 const Products = () => {
-  const id = parseInt(useParams().id);
+  const categoryId = parseInt(useParams().id);
+
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [sort, setSort] = useState(null);
 
   return (
-    <div className="">
-      <div>
-        <div>
-          <h2>Product Categories</h2>
+    <div className="px-10 pt-5 flex gap-4 w-full">
+      <div className=" w-1/5">
+        <div className="mb-5">
+          <h2 className="text-2xl font-semibold">Product Categories</h2>
           <div>
-            <input type="checkbox" id="1" value={1} />
+            <input className="mx-1" type="checkbox" id="1" value={1} />
             <label htmlFor="1">Shoes</label>
           </div>
           <div>
-            <input type="checkbox" id="2" value={2} />
+            <input className="mx-1" type="checkbox" id="2" value={2} />
             <label htmlFor="2">Skirts</label>
           </div>
           <div>
-            <input type="checkbox" id="3" value={3} />
+            <input className="mx-1" type="checkbox" id="3" value={3} />
             <label htmlFor="3">Coats</label>
           </div>
         </div>
-        <div>
-          <h2>Filter by price</h2>
+        <div className="mb-5">
+          <h2 className="text-2xl font-semibold">Filter by price</h2>
           <div>
-            <span>0</span>
-            <input type="range" min={0} max={1000} />
-            <span>1000</span>
+            <span className="mx-1">0</span>
+            <input
+              type="range"
+              min={0}
+              max={1000}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
+            <span className="mx-1">{maxPrice}</span>
           </div>
         </div>
-        <div>
-          <h2>Sort by</h2>
+        <div className="mb-5">
+          <h2 className="text-2xl font-semibold">Sort by</h2>
           <div>
-            <input type="radio" id="asc" value="asc" name="price" />
+            <input
+              className="mx-1"
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort("asc")}
+            />
             <label htmlFor="asc">Price (Lowest first)</label>
           </div>
           <div>
-            <input type="radio" id="asc" value="asc" name="price" />
-            <label htmlFor="asc">Price (Highest first)</label>
+            <input
+              className="mx-1"
+              type="radio"
+              id="desc"
+              value="desc"
+              name="price"
+              onChange={(e) => setSort("desc")}
+            />
+            <label htmlFor="desc">Price (Highest first)</label>
           </div>
         </div>
       </div>
@@ -49,7 +72,7 @@ const Products = () => {
           src="https://images.pexels.com/photos/341392/pexels-photo-341392.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="products background"
         />
-        <List />
+        <List categoryId={categoryId} maxPrice={maxPrice} sort={sort} />
       </div>
     </div>
   );
