09d830779b5289adfdaee940279744a74ec197b5
Jfnrosimo <j.rosimo098.com>
2023-01-15 10:22:41 +0800

    Feat: final touch on categories grid images

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -5,55 +5,79 @@ const Categories = () => {
     <div className="mx-32 h-[80vh] flex gap-2">
       <div className="flex w-1/2 gap-2 h-full">
         <div className="flex flex-col gap-2">
-          <div className="relative h-1/2">
+          <div className="relative h-1/2 flex justify-center items-center">
             <img
               className="object-cover h-full w-full"
               src="https://images.pexels.com/photos/12189048/pexels-photo-12189048.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="shoes"
             />
-            <Link className="absolute" to="">
+            <Link
+              className="absolute px-2 bg-slate-100 rounded-sm hover:bg-blue-500 hover:text-slate-100"
+              to=""
+            >
               WOMEN
             </Link>
           </div>
-          <div>
+          <div className="relative h-1/2 flex justify-center items-center">
             <img
+              className="object-cover h-full w-full"
               src="https://images.pexels.com/photos/5585841/pexels-photo-5585841.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="shoes"
             />
-            <Link to="">SALE</Link>
+            <Link
+              className="absolute px-2 bg-slate-100 rounded-sm hover:bg-blue-500 hover:text-slate-100"
+              to=""
+            >
+              SALE
+            </Link>
           </div>
         </div>
-        <div>
+        <div className="relative h-full flex justify-center items-center">
           <img
+            className="object-cover h-full w-full"
             src="https://images.pexels.com/photos/2923157/pexels-photo-2923157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
           />
-          <Link to="/">NEW SEASON</Link>
+          <Link
+            className="absolute px-2 bg-slate-100 rounded-sm hover:bg-blue-500 hover:text-slate-100"
+            to="/"
+          >
+            NEW SEASON
+          </Link>
         </div>
       </div>
-      <div className="flex flex-col w-1/2">
-        <div className="flex">
-          <div>
+      <div className="flex flex-col w-1/2 gap-2">
+        <div className="flex h-1/2 gap-2">
+          <div className="relative w-1/2 h-full flex justify-center items-center">
             <img
+              className="object-cover h-full w-full"
               src="https://images.pexels.com/photos/716411/pexels-photo-716411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt=""
             />
-            <Link>MEN</Link>
+            <Link className="absolute px-2 bg-slate-100 rounded-sm hover:bg-blue-500 hover:text-slate-100">
+              MEN
+            </Link>
           </div>
-          <div>
+          <div className="relative w-1/2 h-full flex justify-center items-center">
             <img
+              className="object-cover h-full w-full"
               src="https://images.pexels.com/photos/935985/pexels-photo-935985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt=""
             />
-            <Link>ACCESSORIES</Link>
+            <Link className="absolute px-2 bg-slate-100 rounded-sm hover:bg-blue-500 hover:text-slate-100">
+              ACCESSORIES
+            </Link>
           </div>
         </div>
-        <div>
+        <div className="relative w-full h-1/2 flex justify-center items-center">
           <img
+            className="object-cover h-full w-full"
             src="https://images.pexels.com/photos/2048548/pexels-photo-2048548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt="shoes"
           />
-          <Link>Shoes</Link>
+          <Link className="absolute px-2 bg-slate-100 rounded-sm hover:bg-blue-500 hover:text-slate-100">
+            Shoes
+          </Link>
         </div>
       </div>
     </div>
