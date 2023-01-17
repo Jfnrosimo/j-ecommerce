5d2ab06473e5978fbf1663ec64aa94edf717b39f
Jfnrosimo <j.rosimo098.com>
2023-01-17 21:12:13 +0800

    Feat: completed API and started fetching data

--- client/src/components/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts.jsx
@@ -1,9 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+//Import component
 import Card from "./Card";
 
 //Import data
 import data from "../Data";
 
 const FeaturedProducts = ({ type }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          process.env.REACT_APP_API_URL + "/products",
+          {
+            headers: {
+              Authorization: "Bearer " + process.env.REACT_APP_API_TOKEN,
+            },
+          }
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="mx-32 my-16">
       <div className="flex justify-between my-6">
