960543fa6e84a5b8abf9851d34fc1c27d9a12879
Jfnrosimo <j.rosimo098.com>
2023-01-20 12:58:13 +0800

    Feat; finalized stripe order functionalities

--- api/src/api/order/controllers/order.js
+++ api/src/api/order/controllers/order.js
@@ -1,6 +1,5 @@
-const stripe = require("stripe")(process.env.STRIPE_KEY);
-
 ("use strict");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 /**
  * order controller
@@ -11,36 +10,36 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async create(ctx) {
     const { products } = ctx.request.body;
-
-    const lineItems = await Promise.all(
-      products.map(async (currentItem) => {
-        const item = await strapi
-          .service("api:product.product")
-          .findOne(currentItem.id);
-
-        return {
-          price_data: {
-            currency: "php",
-            product_data: {
-              name: item.title,
-            },
-            unit_amout: item.price * 100,
-          },
-          quantity: item.quantity,
-        };
-      })
-    );
     try {
-      const session = stripe.checkout.create({
+      const lineItems = await Promise.all(
+        products.map(async (currentItem) => {
+          const item = await strapi
+            .service("api::product.product")
+            .findOne(currentItem.id);
+
+          return {
+            price_data: {
+              currency: "php",
+              product_data: {
+                name: item.title,
+              },
+              unit_amount: Math.round(item.price * 100),
+            },
+            quantity: currentItem.quantity,
+          };
+        })
+      );
+
+      const session = await stripe.checkout.sessions.create({
         mode: "payment",
-        success_url: `${process.env.CLIENT_URL}?success=true`,
-        cancel_url: `${process.env.CLIENT_URL}?success=false`,
+        success_url: process.env.CLIENT_URL + "?success=true",
+        cancel_url: process.env.CLIENT_URL + "?success=false",
         line_items: lineItems,
         shipping_address_collection: { allowed_countries: ["US", "CA", "PH"] },
         payment_method_types: ["card"],
       });
 
-      await strapi.service("api::order:order").create({
+      await strapi.service("api::order.order").create({
         data: {
           products,
           stripeId: session.id,
@@ -50,7 +49,7 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       return { stripeSession: session };
     } catch (error) {
       ctx.response.status = 500;
-      return error;
+      return { error };
     }
   },
 }));
